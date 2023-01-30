230ef6726c72ad13dc4ce4dee8b099d934897ec8
HunterHarvell <hunterharvell95@gmail.com>
2023-01-30 13:47:22 -0600

    working on fixing JS

--- assets/script.js
+++ assets/script.js
@@ -1,67 +1,95 @@
+// variables for page elements
+// time and score
+let timerEl = document.getElementById("timer");
+let secondsLeft = 75;
+let scoreEl = document.getElementById("#score");
+
+// start prompt
+const startPrompt = document.getElementById('#start');
+
+// quiz section
+const quizEl = document.getElementById('#quiz');
+let question = document.getElementById('#question');
+let questionCount = 0;
+const answerCheck = document.getElementById('#answerCheck');
+
+// quiz end
+const enterHS = document.getElementById('#enterHS');
+let initialsInput = document.getElementById('#initials');
+
+// highscores
+const highscores = document.getElementById('#highscores');
+let scoreList = document.getElementById('#score-list');
+let coreList = [];
+
+// buttons
+const startBtn = document.getElementById('#start-quiz-btn');
+
+const ansBtn = document.querySelectorAll('button.ansBtn');
+
+const ansBtn1 = document.getElementById('#btn1');
+
+const ansBtn2 = document.getElementById('#btn2');
+
+const ansBtn3 = document.getElementById('#btn3');
+
+const ansBtn4 = document.getElementById('#btn4');
+
+const submitScoreBtn = document.getElementById('#submit-score');
+
+const goBackBtn = document.getElementById('#goback');
+
+const clearScrBtn = document.getElementById('#clearscores');
+
+const viewHSBtn = document.getElementById('#viewHighscore');
+
+
 var headerDiv = document.getElementById('header');
-var timerEl = document.getElementById('timer');
 var displayDiv = document.getElementById('display');
-var questionEl = document.getElementById('question');
-var startPrompt = document.getElementById('startPrompt');
+
+
 var response = document.getElementById("response");
 
 // Question array
-let questions = [
+const questions = [
     {
         question : "Commonly used data types do NOT include:",
-        choiceA : "1. boolean",
-        choiceB : "2. number",
-        choiceC : "3. strings",
-        choiceD : "4. alerts",
-        correct : "A"
+        answers : ["1. boolean", "2. number", "3. strings", "4. alerts"],
+        correctAnswer : "1"
     },
     {
         question : "A very useful tool used during development and debugging for printing content to the debugger is:",
-        choiceA : "1. JavaScript",
-        choiceB : "2. terminal/bash",
-        choiceC : "3. for loops",
-        choiceD : "4. console.log",
-        correct : "D"
+        answers : ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        correctAnswer : "4"
     },
     {
         question : "The condition in an if / else statement is enclosed with:",
-        choiceA : "1. quotes",
-        choiceB : "2. curly brackets",
-        choiceC : "3. parenthesis",
-        choiceD : "4. square brackets",
-        correct : "C"
+        answers : ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"],
+        correctAnswer : "3"
     },
     {
         question : "Arrays in JavaScript can be used to store:",
-        choiceA : "1. numbers and strings",
-        choiceB : "2. other arrays",
-        choiceC : "3. booleans",
-        choiceD : "4. all of the above",
-        correct : "D"
+        answers : ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswer : "4"
     },
     {
         question : "Inside what HTML element do we put JavaScript?",
-        choiceA : "<p>",
-        choiceB : "<script>",
-        choiceC : "<scr>",
-        choiceD : "<div>",
-        correct : "B"
+        answers : ["<p>", "<script>", "<scr>", "<div>"],
+        correctAnswer : "2"
     }
 ]
 
 // Timer
-function startTimer() {
-    var timer;
-    timerCount = 75;
-    // Sets timer
-    timer = setInterval(function() {
-      timerCount--;
-      timerEl.textContent = "Time Remaining: " + timerCount;
+function setTimer() {
+    let timerInterval = setInterval(function() {
+      secondsLeft--;
+      timerEl.textContent = 'Time Remaining: ${secondsLeft}';
       // Tests if time has run out
-      if (timerCount === 0) {
-        // Clears interval
-        clearInterval(timer);
-        endGame();
+      if (secondsLeft === 0 || questionCount === questions.length) {
+        clearInterval(timerInterval);
+        quizEl.style.display = "none";
+        enterHS.style.display = "block";
+        scoreEl.textContent = secondsLeft;
       }
     }, 1000);
 }
